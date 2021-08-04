6c2af65dfb76051ee51390e7ecadcd28c126f94e
BalaRohanKotni <balarohan.k@gmail.com>
2021-08-04 10:31:36 +0530

    made changes to package.json and netword_stats_logger.js to run speedtest every 30mins

--- controller/speedtest.js
+++ controller/speedtest.js
@@ -13,37 +13,41 @@ if (!fs.existsSync("ookla/")) {
 }
 
 
-function processResultAndStore(json) {
-  console.log(json);
-  let dateObj = new Date();
+function runProcessSaveSpeedtest() {
+  function processResultAndStore(json) {
+    console.log(json);
+    let dateObj = new Date();
 
-  const speedtestResult = SpeedtestResult();
+    const speedtestResult = SpeedtestResult();
 
-  speedtestResult.time = `${dateObj.toLocaleString('default', { month: 'long' })} ${dateObj.getDate()} ${dateObj.getFullYear()} - ${dateObj.getHours()}:${dateObj.getMinutes()}`;
+    speedtestResult.time = `${dateObj.toLocaleString('default', { month: 'long' })} ${dateObj.getDate()} ${dateObj.getFullYear()} - ${dateObj.getHours()}:${dateObj.getMinutes()}`;
 
-  speedtestResult.latency = json.ping.latency;
-  speedtestResult.jitter = json.ping.jitter;
-  speedtestResult.downloadBandwidth = parseInt(json.download.bandwidth) / 125000;
-  speedtestResult.uploadBandwidth = parseInt(json.upload.bandwidth) / 125000;
-  speedtestResult.isp = json.isp;
-  speedtestResult.isVpn = json.interface.isVpn;
-  speedtestResult.serverLocation = json.server.location;
-  speedtestResult.serverCountry = json.server.country;
-  speedtestResult.resultURL = json.result.url;
-  speedtestResult.packetLoss = parseInt(json.packetLoss);
+    speedtestResult.latency = json.ping.latency;
+    speedtestResult.jitter = json.ping.jitter;
+    speedtestResult.downloadBandwidth = parseInt(json.download.bandwidth) / 125000;
+    speedtestResult.uploadBandwidth = parseInt(json.upload.bandwidth) / 125000;
+    speedtestResult.isp = json.isp;
+    speedtestResult.isVpn = json.interface.isVpn;
+    speedtestResult.serverLocation = json.server.location;
+    speedtestResult.serverCountry = json.server.country;
+    speedtestResult.resultURL = json.result.url;
+    speedtestResult.packetLoss = parseInt(json.packetLoss);
 
-  speedtestResult.save()
-  console.log("saved")
+    speedtestResult.save()
+    console.log("saved")
+  }
+
+
+  console.log("executing");
+  exec("./ookla/speedtest -f json", function (err, data, getter) {
+    if (err) {
+      console.log("Error", err.message);
+      return;
+    }
+    console.log("done, saving");
+    result = JSON.parse(data.trim())
+    processResultAndStore(result);
+  });
 }
 
-
-console.log("executing");
-exec("./ookla/speedtest -f json", function (err, data, getter) {
-  if (err) {
-    console.log("Error", err.message);
-    return;
-  }
-  console.log("done, saving");
-  result = JSON.parse(data.trim())
-  processResultAndStore(result);
-});
+module.exports = runProcessSaveSpeedtest;
--- network_stats_logger.js
+++ network_stats_logger.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const runProcessSaveSpeedtest = require('./controller/speedtest')
 
 mongoDBPort = 2002
 
@@ -6,3 +7,8 @@ mongoose.connect('mongodb://localhost:2002', { useNewUrlParser: true }, function
     console.log('connected to db');
 });
 
+runProcessSaveSpeedtest();
+
+setInterval(function () {
+    runProcessSaveSpeedtest();
+}, 180000);
