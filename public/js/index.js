875b2efd542833e155c50e8eba2e14e38f4fa25f
BalaRohanKotni <balarohan.k@gmail.com>
2021-08-11 09:39:31 +0530

    tried bar chart, i guess i will stick with it.

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,8 @@
 const CHART = document.getElementById("chart").getContext("2d");
 
-const labelsTime = [];
-const downloadData = [];
-const uploadData = [];
+let labelsTime = [];
+let downloadData = [];
+let uploadData = [];
 
 results.forEach(function (result) {
     downloadData.push(result.downloadBandwidth);
@@ -10,34 +10,60 @@ results.forEach(function (result) {
     labelsTime.push(result.time);
 });
 
+labelsTime = labelsTime.slice(-24);
+downloadData = downloadData.slice(-24);
+uploadData = uploadData.slice(-24);
+
 console.log(labelsTime.length);
-let lineChart = new Chart(CHART, {
-    type: 'line',
-    data: {
-        labels: labelsTime,
-        datasets: [{
-            tension: 0.1,
-            label: 'Download Bandwidth',
-            data: downloadData,
-            fill: true,
-            borderWidth: 2,
-            backgroundColor: 'rgba(31, 168, 231, 0.3)',
-            borderColor: '#1fa8e7',
-            pointBorderWidth: 1,
-            pointHitRadius: 1,
-            pointHoverBorderWidth: 1,
-            pointHoverHitRadius: 1,
-        }, {
-            tension: 0.1,
-            label: 'Upload Bandwidth',
-            data: uploadData,
-            fill: true,
-            borderColor: '#bd71ff',
-            backgroundColor: 'rgba(189, 113, 255, 0.4)',
-            pointBorderWidth: 1,
-            pointHitRadius: 1,
-            pointHoverBorderWidth: 1,
-            pointHoverHitRadius: 1,
-        },]
-    },
-});
+
+let barChart = new Chart(CHART,
+    {
+        type: "bar",
+        data: {
+            labels: labelsTime,
+            datasets: [
+                {
+                    label: "Download Bandwidth",
+                    backgroundColor: "#1fa8e7",
+                    data: downloadData,
+                },
+                {
+                    label: "Upload Bandwidth",
+                    backgroundColor: "#bd71ff",
+                    data: uploadData,
+                }
+            ],
+        },
+    }
+);
+
+// let lineChart = new Chart(CHART, {
+//     type: 'line',
+//     data: {
+//         labels: labelsTime,
+//         datasets: [{
+//             tension: 0.1,
+//             label: 'Download Bandwidth',
+//             data: downloadData,
+//             fill: true,
+//             borderWidth: 2,
+//             backgroundColor: 'rgba(31, 168, 231, 0.3)',
+//             borderColor: '#1fa8e7',
+//             pointBorderWidth: 1,
+//             pointHitRadius: 1,
+//             pointHoverBorderWidth: 1,
+//             pointHoverHitRadius: 1,
+//         }, {
+//             tension: 0.1,
+//             label: 'Upload Bandwidth',
+//             data: uploadData,
+//             fill: true,
+//             borderColor: '#bd71ff',
+//             backgroundColor: 'rgba(189, 113, 255, 0.4)',
+//             pointBorderWidth: 1,
+//             pointHitRadius: 1,
+//             pointHoverBorderWidth: 1,
+//             pointHoverHitRadius: 1,
+//         },]
+//     },
+// });
