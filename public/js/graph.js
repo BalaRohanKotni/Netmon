d0d3aaa5419c99b21ee07d49ba19589f49aebcfd
BalaRohanKotni <balarohan.k@gmail.com>
2021-10-19 22:44:23 +0530

    Added one day back function.

--- public/js/graph.js
+++ public/js/graph.js
@@ -1,50 +1,28 @@
 // variable: results  is declared in index.ejs
 
-const CHART = document.getElementById("chart")
-const MONTHS = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+const CHART = document.getElementById("chart");
+const MONTHS = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
 
+let monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-function getObjByDate(date) {
-    // Test Case: date = Aug 12 2021 - 12:3
-    let month = date.split(' - ')[0].split(' ')[0].substr(0, 3);
-    let day = date.split(' - ')[0].split(' ')[1];
-    let year = date.split(' - ')[0].split(' ')[2];
-
-    let hours = date.split(' - ')[1].split(':')[0];
-    let mins = date.split(' - ')[1].split(':')[1];
-
-    return {
-        month: parseInt(MONTHS.indexOf(month) + 1),
-        day: parseInt(day),
-        year: parseInt(year),
-        hours: parseInt(hours),
-        mins: parseInt(mins),
-    };
-}
-function goBackOneDay(date) {
-
-    date = getObjByDate(date);
-
-    let monthDayNum = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    let month = date.month;
-    let day = parseInt(date.day);
-    let year = date.year;
-
-    let hour = date.hours;
-    let min = date.mins;
-
-    if (day != 1) {
-        return `${MONTHS[month - 1]} ${day - 1} ${year} - ${hour}:${min}`
-    }
-    else if (day == 1 && month != 1) {
-        return `${MONTHS[month - 2]} ${monthDayNum[month - 2]} ${year} - ${hour}:${min}`
-    }
-    else if (day == 1 && month == 1) {
-        return `Dec ${monthDayNum[12 - 2]} ${year - 1} - ${hour}:${min}`
-    }
+if (new Date().getFullYear() % 4 == 0) {
+  monthDays = 29;
 }
 
-let latest50results = results.slice(-50).reverse()
+let latest50results = results.slice(-50).reverse();
 
 console.log(results.length);
 
@@ -53,53 +31,94 @@ let downloadData = [];
 let uploadData = [];
 let urls = [];
 
-// iterate the list latest50results backwards to be efficient
+function oneDayBack(theDate){
+  let [month, date, year] = [theDate.getMonth(), theDate.getDate(), theDate.getFullYear()].map(x => Number.parseInt(x));
+  
+  // date handling
+  if(date!==1){
+       date--;
+  }
+  else{
+      month--;
+      date = monthDays[month];
+      if(month==-1){
+          date = monthDays[11];
+      }
+  }
+  
+  // leap year handling
+  if(year%4==0){
+      if(month==1 && date==28){
+          date = 29;
+      }
+  }
+  
+  // year handling
+  if(month==-1){
+      year--;
+      month = 11;
+  }
+  return [month+1, date, year];
+  }
+
+let currentTime = new Date();
+
 for (let i = latest50results.length - 1; i >= 0; i--) {
+  let result = latest50results[i];
 
-    let result = latest50results[i];
+  downloadData.push(result.downloadBandwidth);
+  uploadData.push(result.uploadBandwidth);
 
-    downloadData.push(result.downloadBandwidth);
-    uploadData.push(result.uploadBandwidth);
+  let dateTime = new Date(result.createdAt);
 
-    // from dateTime remove date and get only time, eg: Aug 12 2021 - 20:12 = 20:12
-    let onlyTime = result.time.split(' - ')[1];
-    labelsTime.push(onlyTime);
+  let onlyTime = dateTime.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  labelsTime.push(onlyTime);
 
-    urls.push(result.resultURL);
+  urls.push(result.resultURL);
 }
 
 Chart.defaults.scale.ticks.beginAtZero = true;
-let barChart = new Chart(CHART,
+let barChart = new Chart(
+  CHART,
 
-    {
-        type: "bar",
-        data: {
-            labels: labelsTime,
-            datasets: [
-                {
-                    label: "Download Bandwidth",
-                    backgroundColor: "#1fa8e7",
-                    data: downloadData,
-                },
-                {
-                    label: "Upload Bandwidth",
-                    backgroundColor: "#bd71ff",
-                    data: uploadData,
-                }
-            ],
+  {
+    type: "bar",
+    data: {
+      labels: labelsTime,
+      // labels: "absdafasaasdas",
+      datasets: [
+        {
+          label: "Download Bandwidth",
+          backgroundColor: "#1fa8e7",
+          data: downloadData,
         },
-        events: ['click'],
-        options: {
-            color: "#fff",
-            maintainAspectRatio: false,
+        {
+          label: "Upload Bandwidth",
+          backgroundColor: "#bd71ff",
+          data: uploadData,
         },
-    }
+      ],
+    },
+    events: ["click"],
+    options: {
+      color: "#fff",
+      maintainAspectRatio: false,
+    },
+  }
 );
 
 function barChartClickHandler(click) {
-    const points = barChart.getElementsAtEventForMode(click, 'nearest', { intersect: true }, true);
-    console.log(urls[points[0].index]);
-    window.open(urls[points[0].index]);
+  const points = barChart.getElementsAtEventForMode(
+    click,
+    "nearest",
+    { intersect: true },
+    true
+  );
+  console.log(urls[points[0].index]);
+  window.open(urls[points[0].index]);
 }
 
 CHART.onclick = barChartClickHandler;
