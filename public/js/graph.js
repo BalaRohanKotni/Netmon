5af2269bdb89ed1b2838808c2e1f0f8d0fe3ccd0
BalaRohanKotni <balarohan.k@gmail.com>
2021-10-20 23:59:45 +0530

    In graph page, changed to display last 24-hours speedtests instead of last 50 speedtest results.

--- public/js/graph.js
+++ public/js/graph.js
@@ -26,59 +26,115 @@ let latest50results = results.slice(-50).reverse();
 
 console.log(results.length);
 
+function oneDayBack(theDate) {
+  let [month, date, year] = [
+    theDate.getMonth(),
+    theDate.getDate(),
+    theDate.getFullYear(),
+  ].map((x) => Number.parseInt(x));
+
+  // date handling
+  if (date !== 1) {
+    date--;
+  } else {
+    month--;
+    date = monthDays[month];
+    if (month == -1) {
+      date = monthDays[11];
+    }
+  }
+
+  // leap year handling
+  if (year % 4 == 0) {
+    if (month == 1 && date == 28) {
+      date = 29;
+    }
+  }
+
+  // year handling
+  if (month == -1) {
+    year--;
+    month = 11;
+  }
+  return [month + 1, date, year];
+}
+
+let currentTime = new Date();
+console.log(currentTime.toLocaleString());
+let oneDayBackDate = new Date(oneDayBack(currentTime));
+
 let labelsTime = [];
 let downloadData = [];
 let uploadData = [];
 let urls = [];
 
-function oneDayBack(theDate){
-  let [month, date, year] = [theDate.getMonth(), theDate.getDate(), theDate.getFullYear()].map(x => Number.parseInt(x));
-  
-  // date handling
-  if(date!==1){
-       date--;
+results.forEach((result) => {
+  if (
+    new Date(result.createdAt).toLocaleDateString() ==
+    oneDayBackDate.toLocaleDateString()
+  ) {
+    if (
+      Number.parseInt(currentTime.getHours()) <=
+        Number.parseInt(new Date(result.createdAt).getHours()) &&
+      Number.parseInt(currentTime.getMinutes()) <=
+        Number.parseInt(new Date(result.createdAt).getMinutes())
+    ) {
+      // labelsTime.push((result.createdAt));
+      labelsTime.push(new Date(result.createdAt).toLocaleTimeString());
+      downloadData.push(result.downloadBandwidth);
+      uploadData.push(result.uploadBandwidth);
+      urls.push(result.resultURL);
+    }
   }
-  else{
-      month--;
-      date = monthDays[month];
-      if(month==-1){
-          date = monthDays[11];
-      }
+  if (
+    new Date(result.createdAt).toLocaleDateString() ==
+    currentTime.toLocaleDateString()
+  ) {
+    if (
+      Number.parseInt(currentTime.getHours()) >=
+        Number.parseInt(new Date(result.createdAt).getHours()) &&
+      Number.parseInt(currentTime.getMinutes()) >=
+        Number.parseInt(new Date(result.createdAt).getMinutes())
+    ) {
+      // labelsTime.push(result.createdAt);
+      labelsTime.push(new Date(result.createdAt).toLocaleTimeString());
+      downloadData.push(result.downloadBandwidth);
+      uploadData.push(result.uploadBandwidth);
+      urls.push(result.resultURL);
+    }
   }
-  
-  // leap year handling
-  if(year%4==0){
-      if(month==1 && date==28){
-          date = 29;
-      }
-  }
-  
-  // year handling
-  if(month==-1){
-      year--;
-      month = 11;
-  }
-  return [month+1, date, year];
-  }
+});
+console.log(labelsTime.length);
 
-let currentTime = new Date();
+// for (let i = latest50results.length - 1; i >= 0; i--) {
 
-for (let i = latest50results.length - 1; i >= 0; i--) {
-  let result = latest50results[i];
+//   let result = latest50results[i];
 
-  downloadData.push(result.downloadBandwidth);
-  uploadData.push(result.uploadBandwidth);
+// downloadData.push(result.downloadBandwidth);
+//   uploadData.push(result.uploadBandwidth);
 
-  let dateTime = new Date(result.createdAt);
+//   let dateTime = new Date(result.createdAt);
 
-  let onlyTime = dateTime.toLocaleTimeString([], {
-    hour: "2-digit",
-    minute: "2-digit",
-  });
-  labelsTime.push(onlyTime);
+//   let iTime = dateTime;
+//   if(iTime.toLocaleDateString() == oneDayBack.toLocaleDateString){
+//     let [iHours, iMinutes] = [iTime.getHours(), iTime.getMinutes()].map(x=>Number.parseInt(x));
+//     let [bHours, bMinutes] = [oneDayBack.getHours(), oneDayBack.getMinutes()].map(x=>Number.parseInt(x));
+//     console.log(iHours, bHours);
+//     if(iHours<=bHours && iMinutes<=bMinutes){
+//     }
+//   }
+//   else if(iTime.toLocaleDateString() == currentTime.toLocaleDateString()){
 
-  urls.push(result.resultURL);
-}
+//   }
+
+//   let onlyTime = dateTime.toLocaleTimeString([], {
+//     hour: "2-digit",
+//     minute: "2-digit",
+//   });
+//   labelsTime.push(onlyTime);
+
+//   urls.push(result.resultURL);
+// }
 
 Chart.defaults.scale.ticks.beginAtZero = true;
 let barChart = new Chart(
@@ -106,6 +162,16 @@ let barChart = new Chart(
     options: {
       color: "#fff",
       maintainAspectRatio: false,
+      // scales: {
+      //   x: {
+
+      //     type: 'time',
+      //     ticks: {
+      //       source: 'auto'
+      //     },
+      //     barThickness: 100,
+      //   }
+      // }
     },
   }
 );
